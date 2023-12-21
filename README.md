d213213b7aebe434d0865a8a4613e69b89e60f51
Eric Rosas <ericdrosas@gmail.com>
2023-12-21 11:25:54 -0700

    Added album select, init func for sensitive info EPO-8896 Added album select, init func for sensitive info

--- README.md
+++ README.md
@@ -3,3 +3,57 @@
 The name of this repo will likely change at some point.
 
 This repo houses the various scripts and tools that can be used to transfer non-citSci data to the EDC. For citSci related tools see the `citizen_science_notebooks` repo.
+
+**Important: The Jupyter notebook in this repo isn't intended to be used for actual data curation, it is only here to demonstrate usage and continued support of the notebook is not planned.**
+
+## Getting Started
+
+You will need the Canto app ID and Canto secret in order to use this script.
+
+First, import and initialize the pipeline script:
+
+```
+import data_pipeline
+
+data_pipeline.initialize("app ID", "app secret")
+```
+
+The pipeline script is self-documenting:
+
+```
+help(data_pipeline)
+```
+
+## Uploading files to Google Cloud Storage
+
+Files of any type can be uploaded, including images.
+
+Given a file that already exists in the RSP notebook filesystem:
+
+```
+url_to_uploaded_file = data_pipeline.upload_to_gcs("./some_data.json", "some_sub_bucket_name")
+url_to_uploaded_file
+```
+
+## Uploading images to Canto
+
+First, identify which album to upload the image to - this is a manual process and involves logging into the Canto platform and navigating to the target album.
+
+Once on the webpage of the target album, take note of the URL, for example:
+
+`https://rubin.canto.com/album/PP4QV?display=fitView&viewIndex=1&gSortingForward=false&gOrderProp=uploadDate&referenceTo=&from=fitView`
+
+Remove everything after and including the `?` for clarity:
+
+`https://rubin.canto.com/album/PP4QV`
+
+The album ID is the top-level section of the URL, in this case `PP4QV`.
+
+With the album ID, upload the image to Canto:
+
+```
+import data_pipeline
+res = data_pipeline.upload_to_canto("./some_sciencey_image.jpg", "PP4QV")
+```
+
+**Unfortunately, the Canto API processes uploads asynchronously and does not provide any information about the URL of the upload. You will need to manually verify that the image has been uploaded.**
--- data_pipeline.py
+++ data_pipeline.py
@@ -3,8 +3,30 @@ import google.cloud.storage as storage
 
 os.environ[base64.b64decode("R09PR0xFX0FQUExJQ0FUSU9OX0NSRURFTlRJQUxT").decode("ascii")] = base64.b64decode("L29wdC9sc3N0L3NvZnR3YXJlL2p1cHl0ZXJsYWIvc2VjcmV0cy9idXRsZXItZ2NzLWlkZi1jcmVkcy5qc29u").decode("ascii")
 
-canto_app_id = "" # Replace with Canto App ID
-canto_secret = "" # Replace with Canto App ID
+def initialize(canto_id, canto_sec):
+    """
+        Sets the Canto app ID and secret for authentication into Canto for asset
+        upload.
+
+        Arguments:
+            canto_id: canto app id
+            canto_sec: canto secret
+
+        Warning:
+            NEVER share or publicly store the canto app ID or secret
+
+        Returns:
+            A message indicating the success or failure of the initialization.
+    """
+    
+    global canto_app_id, canto_secret
+    if canto_id is None or canto_sec is None:
+        print("Both canto ID and canto secret arguments must be provided!")
+        return
+    canto_app_id = canto_id
+    canto_secret = canto_sec
+    print("Data pipeline successfully initialized!")
+    return
 
 def upload_to_gcs(path_to_file, bucket_name):
     """
@@ -30,7 +52,6 @@ def upload_to_gcs(path_to_file, bucket_name):
 
     filename = path_to_file.split("/")[-1]
         
-    
     try:
         storage_client = storage.Client()
         bucket = storage_client.bucket("data_pipeline_uploads")
@@ -42,13 +63,18 @@ def upload_to_gcs(path_to_file, bucket_name):
     except:
         return "An error occurred while attempting to upload the dataset!"
 
-def upload_to_canto(path_to_file):
+def upload_to_canto(path_to_file, canto_album_id):
     """
         Uploads an image to Canto.
 
         Arguments:
             path_to_file: the absolute or relative path to the file
                 * The path can be relative or absolute
+            canto_album_id: the five character alphanumeric ID of the album
+                * https://rubin.canto.com/album/PP4QV?display=fitView&vie...
+                    ** In the above URL the ID can be found in the URL immediately 
+                       after the '/album/' section of the URL before the '?', such
+                       as PP4QV in the above example
 
         Prints a message upon successful upload or failure.
 
@@ -56,14 +82,15 @@ def upload_to_canto(path_to_file):
             None
     """
 
+    global canto_app_id, canto_secret
     response_message = "An error occurred while attempting to upload the image to Canto!"
     try:
         if canto_app_id == "":
-            print("Please ensure that the variable `canto_app_id` is set in data_pipeline.py before calling this function!")
+            print("Please ensure that the variable `canto_app_id` is set in data_pipeline.py before calling this function! You can do so by calling the initialize() function. Run: help(data_pipeline) for more info.")
             return
     
         if canto_secret == "":
-            print("Please ensure that the variable `canto_secret` is set in data_pipeline.py before calling this function!")
+            print("Please ensure that the variable `canto_secret` is set in data_pipeline.py before calling this function! You can do so by calling the initialize() function. Run: help(data_pipeline) for more info.")
             return
     
         auth_url = 'https://oauth.canto.com/oauth/api/oauth2/token?app_id=' + canto_app_id + '&app_secret=' + canto_secret + '&grant_type=client_credentials&refresh_token='
@@ -87,11 +114,10 @@ def upload_to_canto(path_to_file):
             "x-amz-meta-tag": "",
             "x-amz-meta-scheme": "",
             "x-amz-meta-id": "",
-            "x-amz-meta-album_id": "PP4QV", # Craft CMS Integration Testing/Test album 1
+            "x-amz-meta-album_id": canto_album_id, 
             "file": open(path_to_file,'rb')
         }
-        
-        
+    
         res3 = requests.post(res_json2["url"], files=upload_data)
         
         if res3.status_code == 200 or res3.status_code == 204:
